ee1325ad562ef8c3e1c23008cfe22b0196deec64
sarahh-h <sghyatt23@gmail.com>
2026-07-06 10:28:58 -0400

    added scaffolding files for api

---
+++ backend-api/src/main/java/com/csc340/TrailBuddy/TrailBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.csc340.TrailBuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrailBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrailBuddyApplication.class, args);
+	}
+
+}
