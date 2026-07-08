db16e3efb8e26eb97b91529c9a5425ef8ded36ca
sarahh-h <sghyatt23@gmail.com>
2026-07-08 11:39:15 -0400

    basis of provider entity class done

---
+++ backend-api/src/main/java/com/csc340/TrailBuddy/Entity/Provider_entity.java
@@ -0,0 +1,48 @@
+package com.csc340.TrailBuddy.Entity;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "providers")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Provider_entity {
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long providerID;
+
+  @Column(nullable = false)
+  private String name;
+
+  @Column(nullable = false, unique = true)
+  private String emailAddress;
+
+  @Column(nullable = false)
+  private String password;
+
+  @Column(nullable = false)
+  private String location;
+
+  public Provider_entity(String name, String email, String password, String location) {
+    this.name = name;
+    emailAddress = email;
+    this.password = password;
+    this.location = location;
+  }
+
+}
