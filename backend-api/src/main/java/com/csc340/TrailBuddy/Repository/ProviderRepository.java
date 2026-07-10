8b9f9f86661a6b1f401a2a98875d22dc32467329
sarahh-h <sghyatt23@gmail.com>
2026-07-10 06:43:48 -0400

    completed providerRepository

---
+++ backend-api/src/main/java/com/csc340/TrailBuddy/Repository/ProviderRepository.java
@@ -0,0 +1,13 @@
+package com.csc340.TrailBuddy.Repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.csc340.TrailBuddy.Entity.Provider;
+
+@Repository
+public interface ProviderRepository extends JpaRepository<Provider, Long> {
+
+  Provider findByEmail(String email);    //to search by email
+
+}
