c843b6947738109a35b73b2abea220253adcea6c
sarahh-h <sghyatt23@gmail.com>
2026-07-22 12:27:39 -0400

    added app controller to have default page be login page

---
+++ backend-api/src/main/java/com/csc340/TrailBuddy/AppController.java
@@ -0,0 +1,22 @@
+package com.csc340.TrailBuddy;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class AppController {
+
+  public AppController(){
+  }
+
+@GetMapping({ "", "/", "/login" })
+public String mainPage() {
+    return "login";
+   }
+
+   @GetMapping("/live")
+  public ResponseEntity<String> pulse() {
+    return ResponseEntity.ok("App is live");
+  }
+}
